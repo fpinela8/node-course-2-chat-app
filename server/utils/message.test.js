da84730bb4c19109c4ef5828a1f31befe9dc2a07
Fabio <fabio.pinela@gmail.com>
2017-07-02 19:23:37 +0100

    Adding GeoLocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -7,6 +7,19 @@ describe('generateMessage', () => {
 
     expect(res.from).toBe('name');
     expect(res.text).toBe('body');
+    expect(res.createdAt).toBeA('number');
+
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate the correct location object', () => {
+
+    var url = "http://www.google.com/maps?q=15,19";
+    var res = generateLocationMessage('name', 15, 19);
+
+    expect(res.from).toBe('name');
+    expect(res.url).toBe(url);
     expect(res.createdAt).toBeA('number');
 
   });
